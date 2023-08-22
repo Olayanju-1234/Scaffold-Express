b6336816f023cc17fa7e00956c187c05c2a21910
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2023-08-22 17:55:55 +0100

    scaffold-express

--- scaffold-express.js
+++ scaffold-express.js
@@ -3,22 +3,29 @@ const path = require('path');
 
 const scaffoldExpress = (projectName) => {
     const targetPath = path.join(process.cwd(), projectName);
-
     fs.mkdirSync(targetPath);
 
     const srcPath = path.join(targetPath, 'src');
-
     fs.mkdirSync(srcPath);
 
     // components
     const componentsPath = path.join(srcPath, 'components');
     fs.mkdirSync(componentsPath);
     
-    const componentFiles = ['index.js', 'index.test.js'];
-    componentFiles.forEach((file) => {
-        const filePath = path.join(componentsPath, file);
-        fs.writeFileSync(filePath, '');
-    });
+    const componentContent = `
+        const express = require('express');
+
+        const router = express.Router();
+
+        router.get('/', (req, res) => {
+            res.send('Hello World!');
+        });
+
+        module.exports = router;
+    `;
+    
+    const componentPath = path.join(componentsPath, 'index.js');
+    fs.writeFileSync(componentPath, componentContent);
 
     // test
     const testPath = path.join(srcPath, 'test');
@@ -35,12 +42,68 @@ const scaffoldExpress = (projectName) => {
     const configPath = path.join(srcPath, 'config');
     fs.mkdirSync(configPath);
 
-    const configFiles = ['database.js', 'default.json', 'development.json', 'production.json'];
-    configFiles.forEach((file) => {
-        const filePath = path.join(configPath, file);
-        fs.writeFileSync(filePath, '');
-    }
-    );
+    const databaseContent = `
+        const database = {
+            development: {
+                username: 'root',
+                password: 'password',
+                database: 'database_development',
+                host: '',
+            },
+
+            test: {
+
+            },
+
+            production: {
+
+            },
+
+        };
+
+        module.exports = database;
+    `;
+    const databasePath = path.join(configPath, 'database.js');
+    fs.writeFileSync(databasePath, databaseContent);
+
+    const defaultContent = `
+        {
+            "database": {
+
+            }
+        }
+    `;
+    const defaultPath = path.join(configPath, 'default.json');
+    fs.writeFileSync(defaultPath, defaultContent);
+
+    const developmentContent = `
+        
+    `;
+    const developmentPath = path.join(configPath, 'development.json');
+    fs.writeFileSync(developmentPath, developmentContent);
+
+    const productionContent = `
+
+    `;
+    const productionPath = path.join(configPath, 'production.json');
+    fs.writeFileSync(productionPath, productionContent);
+
+    const configIndexContent = `
+        const database = require('./database');
+        const defaultConfig = require('./default.json');
+        const developmentConfig = require('./development.json');
+        const productionConfig = require('./production.json');
+
+        module.exports = {
+            database,
+            defaultConfig,
+            developmentConfig,
+            productionConfig,
+        };
+    `;
+
+    const configIndexPath = path.join(configPath, 'index.js');
+    fs.writeFileSync(configIndexPath, configIndexContent);
 
     // utils
     const utilsPath = path.join(srcPath, 'utils');
@@ -63,6 +126,45 @@ const scaffoldExpress = (projectName) => {
         fs.writeFileSync(filePath, '');
     }
     );
+
+    // middlewares
+    const middlewaresPath = path.join(srcPath, 'middlewares');
+    fs.mkdirSync(middlewaresPath);
+
+    const middlewaresFiles = ['index.js'];
+    middlewaresFiles.forEach((file) => {
+        const filePath = path.join(middlewaresPath, file);
+        fs.writeFileSync(filePath, '');
+    }
+    );
+
+    // errors
+    const errorsPath = path.join(srcPath, 'errors');
+    fs.mkdirSync(errorsPath);
+
+    const customErrorContent = `
+        class CustomError extends Error {
+            constructor(message, statusCode) {
+                super(message);
+                this.statusCode = statusCode;
+            }
+        }
+
+        module.exports = CustomError;
+    `;
+    const customErrorPath = path.join(errorsPath, 'CustomError.js');
+    fs.writeFileSync(customErrorPath, customErrorContent);
+
+    const errorsIndexContent = `
+        const CustomError = require('./CustomError');
+
+        module.exports = {
+            CustomError,
+        };
+    `;
+    const errorsIndexPath = path.join(errorsPath, 'index.js');
+    fs.writeFileSync(errorsIndexPath, errorsIndexContent);
+
 
     // app.js
     const appContent = `
